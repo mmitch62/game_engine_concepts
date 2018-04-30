4ecaa2385a66d53b11ea0fa8dad6f5fda6f48cbf
Kyle Conel <kyleconel@yahoo.com>
2018-04-30 14:47:49 -0400

    Added CEGUI and Ogres that follow the player.

---
+++ ClassProject/ClassProject/OgreHead.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "BaseApplication.h"
+#include <Terrain/OgreTerrain.h>
+#include <Terrain/OgreTerrainGroup.h>
+#include "OgreManualObject.h"
+#include "btBulletDynamicsCommon.h"
+#include "BulletCollision\CollisionShapes\btHeightfieldTerrainShape.h"
+#include "BulletCollision\CollisionShapes\btCollisionShape.h"
+#include "BaseApplication.h"
+#include <CEGUI/CEGUI.h>
+#include <CEGUI/RendererModules/Ogre/Renderer.h>
+#include <vector>
+//this struct is used to hold info about the ogre heads
+
+static std::vector<Ogre::Vector3> moveList;
+
+struct OgreHeadStruct {
+	Ogre::SceneNode* ogreNode;
+	Ogre::Vector3 destination;
+	Ogre::Vector3 direction;
+	Ogre::Real distance;
+	std::vector<Ogre::Vector3>::iterator nextSpot;
+	btRigidBody * ogreBody;
+	Ogre::Real shootTimer;
+
+	//constructors
+	OgreHeadStruct() {
+		destination = Ogre::Vector3::ZERO;
+		direction = Ogre::Vector3::ZERO;
+		distance = 0;
+		nextSpot = moveList.begin();
+		shootTimer = 0;
+	}
+
+	OgreHeadStruct(Ogre::SceneNode* node, btRigidBody *body) {
+		ogreNode = node;
+		destination = Ogre::Vector3::ZERO;
+		direction = Ogre::Vector3::ZERO;
+		distance = 0;
+		nextSpot = moveList.begin();
+		ogreBody = body;
+		shootTimer = 0;
+	}
+
+	void increm() {
+		nextSpot++;
+	}
+};
--- ClassProject/ClassProject/ProjectApplication.h
+++ ClassProject/ClassProject/ProjectApplication.h
@@ -2,6 +2,41 @@
 #define __ProjectApplication_h_
 
 #include "BaseApplication.h"
+#include "OgreHead.h"
+
+class MyMotionState : public btMotionState {
+		public:
+			        MyMotionState(const btTransform &initialpos, Ogre::SceneNode *node) {
+				                mVisibleobj = node;
+				                mPos1 = initialpos;
+			
+		}
+			        virtual ~MyMotionState() {    }
+			        void setNode(Ogre::SceneNode *node) {
+				                mVisibleobj = node;
+			
+		}
+			        virtual void getWorldTransform(btTransform &worldTrans) const {
+				                worldTrans = mPos1;
+			
+		}
+			        virtual void setWorldTransform(const btTransform &worldTrans) {
+				                if (NULL == mVisibleobj) return; // silently return before we set a node
+				                btQuaternion rot = worldTrans.getRotation();
+				                mVisibleobj->setOrientation(rot.w(), rot.x(), rot.y(), rot.z());
+				                btVector3 pos = worldTrans.getOrigin();
+				                // TODO **** XXX need to fix this up such that it renders properly since this doesnt know the scale of the node
+					                // also the getCube function returns a cube that isnt centered on Z
+					                mVisibleobj->setPosition(pos.x(), pos.y() + 5, pos.z() - 5);
+			
+		}
+			protected:
+				        Ogre::SceneNode *mVisibleobj;
+				        btTransform mPos1;
+			
+};
+
+
 
 class ProjectApplication : public BaseApplication
 {
@@ -12,9 +47,16 @@ public:
 protected:
 	virtual void createScene(void);
 	virtual bool frameRenderingQueued(const Ogre::FrameEvent& fe);
-
+	void CreateBall(const btVector3 &Position, btScalar Mass, const btVector3 &scale, std::string name, Ogre::Vector3 direction);
+	void CreateOgre(const btVector3 &Position);
+	bool frameStarted(const Ogre::FrameEvent &evt);
+	//Ogre::ManualObject *createCubeMesh(Ogre::String name, Ogre::String matName);
+	CEGUI::OgreRenderer* mRenderer;
+	bool quitGame(const CEGUI::EventArgs &e);
 private:
+	void createBulletSim(void);
 	bool processUnbufferedInput(const Ogre::FrameEvent& fe);
+
 
 	Ogre::SceneNode* mMainNode;
 	Ogre::SceneNode* mPlayerNode;
@@ -24,8 +66,57 @@ private:
 	Ogre::AnimationState* mPlayerAnimation;
 
 	Ogre::Real mPlayerSpd;
+	Ogre::Real ogreMove;
+	Ogre::Real mRotate;
+	Ogre::Real mMove;
+	Ogre::Vector3 mDirection;
+	//timers and variables to control how many enemies there are
+	double timer, respawnTime;
+	int numOgres; int maxOgres;
 
 	bool isPlayerMoving;
-};
+	double sphereVelocity; int sphereIt;
+	std::vector<OgreHeadStruct> ogreHeads;
 
+	OgreBites::Label* mInfoLabel;
+	btDefaultCollisionConfiguration* collisionConfiguration;
+	btCollisionDispatcher* dispatcher;
+	btBroadphaseInterface* overlappingPairCache;
+	btSequentialImpulseConstraintSolver* solver;
+	btDiscreteDynamicsWorld* dynamicsWorld;
+	btCollisionShape* groundShape;
+	btAlignedObjectArray<btCollisionShape*> collisionShapes;
+
+
+	int ninjaHealth = 10;
+
+	CEGUI::Window *healthWindow;
+
+struct ogreObject {
+	Ogre::Entity * entityObject;
+	 Ogre::SceneNode * sceneNodeObject;
+	 MyMotionState * myMotionStateObject;
+	btCollisionShape * btCollisionShapeObject;
+	                btRigidBody * btRigidBodyObject;
+	                btCollisionObject * btCollisionObjectObject;
+		                std::string objectType;
+	               Ogre::Real timer = 0;
+	               bool canDelete = false;
+	      OgreHeadStruct headStruct;
+	 int health = 3;
+
+};
+ogreObject* getOgreObject(const btCollisionObject * obj);
+
+	        ogreObject * ptrToOgreObject; // pointer to an ogre Object struct
+	        std::vector<ogreObject *> ptrToOgreObjects; // vector of pointers to actual ogreObject structs
+
+	        ogreObject* ninjaObject;
+
+	        void checkCollisions();
+	        void checkDeletions();
+	        void removeDynamicOgreObject(ogreObject * ptrToOgreObject, std::vector<ogreObject *> &ptrToOgreObjects);
+	       void eraseObject(ogreObject * object);
+	       // void removeOgreHeadObject(OgreHeadStruct headStruct, std::vector<OgreHeadStruct> &ogreHeads);
+};
 #endif
